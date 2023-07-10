eda65b8c52d0990a2323e8d35a9ad2a0da4a3384
MilenLefterov <m.lefterov95@gmail.com>
2023-07-10 14:38:11 +0300

    Lab 7 - solution
    Task 3 solution

---
+++ Java_Fundamentals/src/Lectures_Labs/Lecture7/P03_OddOccurrences.java
@@ -0,0 +1,31 @@
+package Lectures_Labs.Lecture7;
+
+import java.util.*;
+
+public class P03_OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = scanner.nextLine()
+                .toLowerCase()
+                .split("\\s+");
+
+        Map<String, Integer> wordCounts = new LinkedHashMap<>();
+
+        for (String currentWord : words) {
+            wordCounts.putIfAbsent(currentWord, 0);
+            wordCounts.put(currentWord, wordCounts.get(currentWord) + 1);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (var entry : wordCounts.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                result.add(entry.getKey());
+            }
+        }
+
+        System.out.printf("%s\n", String.join(", ", result));
+
+    }
+}
