d3a25fb4a749a4f5d23ac10252c6f8973f083438
MilenLefterov <m.lefterov95@gmail.com>
2023-07-22 15:38:15 +0300

    Lab 9 - P2 solution

---
+++ Java_Fundamentals/src/Lectures_Labs/Lecture9/P02_MatchPhoneNumber.java
@@ -0,0 +1,26 @@
+package Lectures_Labs.Lecture9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P02_MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        String regexForNames = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+        Pattern pattern = Pattern.compile(regexForNames);
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> matchedPhoneNumbers = new ArrayList<>();
+
+        while (matcher.find()) {
+            matchedPhoneNumbers.add(matcher.group());
+        }
+        System.out.println(String.join(", ", matchedPhoneNumbers));
+    }
+}
