fe98fc541c67211ea0605357bc61145378bb695f
MilenLefterov <m.lefterov95@gmail.com>
2023-09-14 11:34:16 +0300

    Lab 1 - P2 solution

---
+++ Java_Advanced/src/Lectures_Labs/Lecture1/P02_SimpleCalculator.java
@@ -0,0 +1,36 @@
+package Lectures_Labs.Lecture1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+//ArrayDeque<Integer> stack = new ArrayDeque<>();
+//stack.push(element);
+//stack.pop();
+//stack.peek();
+
+public class P02_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        String[] tokens = scanner.nextLine().split("\\s+");
+
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            stack.push(tokens[i]);
+        }
+
+        while (stack.size() > 1) {
+            int firstN = Integer.parseInt(stack.pop());
+            String op = stack.pop();
+            int secondN = Integer.parseInt(stack.pop());
+
+            if (op.equals("+")) {
+                stack.push(String.valueOf(firstN + secondN));
+            } else {
+                stack.push(String.valueOf(firstN - secondN));
+            }
+        }
+
+        System.out.println(stack.peek());
+    }
+}
