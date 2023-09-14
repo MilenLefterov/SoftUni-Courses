3e68c519e5ce8ba2694fe56cd388331f4cf52a38
MilenLefterov <m.lefterov95@gmail.com>
2023-09-14 11:56:56 +0300

    Lab 1 - P4 solution

---
+++ Java_Advanced/src/Lectures_Labs/Lecture1/P04_MatchingBrackets.java
@@ -0,0 +1,22 @@
+package Lectures_Labs.Lecture1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P04_MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> startIndexStack = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                startIndexStack.push(i);
+            } else if (input.charAt(i) == ')') {
+                System.out.println(input.substring(startIndexStack.pop(), i + 1));
+            }
+        }
+    }
+}
