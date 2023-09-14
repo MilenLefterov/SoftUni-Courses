c1875cd4963e389225de0e6db727cc77d93d6dce
MilenLefterov <m.lefterov95@gmail.com>
2023-09-14 16:39:34 +0300

    Lab 1 - P8 solution

---
+++ Java_Advanced/src/Lectures_Labs/Lecture1/P08_BrowserHistoryUpgrade.java
@@ -0,0 +1,40 @@
+package Lectures_Labs.Lecture1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P08_BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String> historyBackStack = new ArrayDeque<>();
+        ArrayDeque<String> historyForwardStack = new ArrayDeque<>();
+
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (historyBackStack.size() > 1) {
+                    historyForwardStack.push(historyBackStack.pop());
+                    System.out.println(historyBackStack.peek());
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else if (input.equals("forward")) {
+                if (historyForwardStack.size() > 0) {
+                    System.out.println(historyForwardStack.peek());
+                    historyBackStack.push(historyForwardStack.pop());
+                } else {
+                    System.out.println("no next URLs");
+                }
+            } else {
+                historyBackStack.push(input);
+                System.out.println(historyBackStack.peek());
+                while (historyForwardStack.size() > 0) {
+                    historyForwardStack.pop();
+                }
+            }
+
+            input = scanner.nextLine();
+        }
+    }
+}
