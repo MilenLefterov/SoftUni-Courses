006a03e01ef172e99a235743a6f0ce95ad7a54d0
MilenLefterov <m.lefterov95@gmail.com>
2023-09-16 21:46:56 +0300

    Еxercise 1 - P1 solution

---
+++ Java_Advanced/src/Exercises/Exercise1/P01_ReverseNumbersWithAStack.java
@@ -0,0 +1,37 @@
+package Exercises.Exercise1;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class P01_ReverseNumbersWithAStack {
+
+    //Stack
+    // push()
+    // pop()
+    // peek()
+
+    //Queue
+    // offer()
+    // poll()
+    // peek()
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int[] inputArr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        for (int e : inputArr) {
+            stack.push(e);
+        }
+
+        while (stack.size() > 0) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
+}
