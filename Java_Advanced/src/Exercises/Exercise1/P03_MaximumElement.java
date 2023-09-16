c99b3972af05395431c581587a509f24a12077b3
MilenLefterov <m.lefterov95@gmail.com>
2023-09-16 22:25:03 +0300

    Еxercise 1 - P3 solution

---
+++ Java_Advanced/src/Exercises/Exercise1/P03_MaximumElement.java
@@ -0,0 +1,30 @@
+package Exercises.Exercise1;
+
+import java.util.*;
+
+public class P03_MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            switch (input[0]) {
+
+                case "1":
+                    stack.push(Integer.parseInt(input[1]));
+                    break;
+                case "2":
+                    stack.pop();
+                    break;
+                case "3":
+                    System.out.println(Collections.max(stack));
+                    break;
+            }
+        }
+    }
+}
