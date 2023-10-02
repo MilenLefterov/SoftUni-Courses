438899ff9bc7e50cb0409d09d3bffbf9c8ce71c6
MilenLefterov <m.lefterov95@gmail.com>
2023-10-02 12:16:50 +0300

    Lab 4 - Streams, Files And Directories - P1 solution

---
+++ Java_Advanced/src/Lectures_Labs/Lecture4_StreamsFilesAndDirectories/P01_ReadFile.java
@@ -0,0 +1,19 @@
+package Lectures_Labs.Lecture4_StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class P01_ReadFile {
+    public static void main(String[] args) throws IOException {
+        String path = "src\\Lectures_Labs\\Lecture4_StreamsFilesAndDirectories\\Files\\input.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        int oneByte = fileInputStream.read();
+        while (oneByte != -1) {
+            System.out.printf("%s ", Integer.toBinaryString(oneByte));
+            oneByte = fileInputStream.read();
+        }
+
+    }
+}
