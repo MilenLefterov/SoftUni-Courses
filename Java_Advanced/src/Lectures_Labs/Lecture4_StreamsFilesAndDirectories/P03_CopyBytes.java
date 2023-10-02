81a25373cf07f9a75a15eb7183c449f737aa41ca
MilenLefterov <m.lefterov95@gmail.com>
2023-10-02 12:52:14 +0300

    Lab 4 - Streams, Files And Directories - P3 solution

---
+++ Java_Advanced/src/Lectures_Labs/Lecture4_StreamsFilesAndDirectories/P03_CopyBytes.java
@@ -0,0 +1,37 @@
+package Lectures_Labs.Lecture4_StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class P03_CopyBytes {
+    public static void main(String[] args) throws IOException {
+
+        String path = "src\\Lectures_Labs\\Lecture4_StreamsFilesAndDirectories\\Files\\input.txt";
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        String pathForResults = "src\\Lectures_Labs\\Lecture4_StreamsFilesAndDirectories\\Files\\Results\\P03_CopyBytes-Result.txt";
+        FileOutputStream fileOutputStream = new FileOutputStream(pathForResults);
+
+        int oneByte = fileInputStream.read();
+        while (oneByte != -1) {
+
+            if (oneByte == 32) {
+                fileOutputStream.write(' ');
+            } else if (oneByte == 10) {
+                fileOutputStream.write('\n');
+            } else {
+                String message = String.valueOf(oneByte);
+
+                for (Character ch : message.toCharArray()) {
+                    fileOutputStream.write(ch);
+                }
+            }
+
+            oneByte = fileInputStream.read();
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
